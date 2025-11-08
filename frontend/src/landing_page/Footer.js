dbb8774f6087df1af7589a480bba60590ead7442
Ganesh Dandekar <ganesh15122002@gmail.com>
2025-11-09 01:11:32 +0530

    Navbar and Footer developed

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -2,7 +2,68 @@ import React from 'react';
 
 function Footer() {
     return ( 
-        <h1>Footer </h1>
+        <footer style={{backgroundColor: 'rgb(250, 250, 250)'}}>
+            <div className="container border-top mt-5">
+                <div className="row mt-5">
+
+                    <div className="col">
+                        <img src="media/images/logo.svg" alt="logo" style={{width: '50%'}} />
+                        <p> &copy; 2022-2025, Stock broker LTD. All rights reserved.</p>
+                    </div>
+
+                    <div className="col">
+                        <p>Company</p>
+                       <a href="" className='text-muted' style={{textDecoration: 'none'}}>About </a> <br /> <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Products </a>  <br />  <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Pricing  </a>  <br />  <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Referral Program </a>  <br />  <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Careers </a>  <br />  <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Stock-broker.tech </a>  <br />  <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Press & media </a>  <br />  <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>StockBroker cares </a>  <br /> 
+                    </div>
+
+                    <div className="col">
+                        <p>Support</p>
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Contact </a> <br />   <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Support portal </a>  <br />   <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>S-connect blog  </a>  <br />   <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>List of charges </a>  <br />  <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Download & resources </a>  <br />   
+
+                    </div>
+
+                    <div className="col">
+                        <p>Account</p>
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Open an account </a> <br />   <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>Fund transfer </a>  <br />   <br />
+                        <a href="" className='text-muted' style={{textDecoration: 'none'}}>60 day challange  </a>  <br />   <br />
+                
+                    </div>
+                </div>
+
+                <div className='mt-5 text-muted' style={{fontSize: '14px'}}>
+                    <p> 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti deserunt error adipisci iste ab incidunt cupiditate. Culpa totam officia natus officiis a? Reprehenderit ab perferendis illo sapiente eligendi maxime distinctio?
+                        Deserunt rerum accuda maxime recusandae quis nulla! Recusandae libero sed delectus excepturi, nam tenetur debitis. Eius, accusamus eveniet quos placeat totam, inventore quas fuga, consequuntur libero incidunt molestias itaque deserunt.
+                        Sit et ipsam dignissimos placeat tenetur? Doloribus inventore minima eos magni maxime ipsam pariatur voluptatum delectus! Nostrum officiis, veritatis totam vitae, ullam itaque necessitatibus sint rerum, excepturi aliquam ab enim.
+                        A incidunt, ipsa dolorum aliquid hic autem distinctio architecto maiores saepe nobis quaerat praesentium velit dicta modi minus enim sed aperiam! Quos facilis impedit ipsum iste neque voluptatem, laboriosam delectus. 
+                    </p>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum incidunt, nisi et tenetur provident inventore fugit ab atque qui distinctio excepturi harum molestias odio molestiae consequatur minima. Officiis, fuga consequuntur.
+                        Quibusdam expedita quam sunt aperiam voluptas omnis. Neque minima aut commodi recusandae! Accusamus expedita praesentium, minus fugit excepturi laborum, totam dolorum blanditiis deserunt molestias sequi temporibus officia aspernatur nostrum quisquam?
+                        Ipsam odit sint nulla idri officia vero, ipsa quidem consequuntur, laudantium sequi vel aut iste, pariatur voluptas eveniet doloremque? Voluptatum magnam nisi quo porro? Aut iusto necessitatibus ratione esse, velit obcaecati!
+                    </p>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ea non ipsam? Nisi ad maxime reprehenderit perspiciatis, veritatis libero nihil cum harum velit labore, eius qui. In veniam nisi tenetur.
+                        Dolores eaque id sequi blanditiis laudantium maxime quia nisi magni, architecto dignissimos assumenda tempore aspernatur dolor accusamus cum! Error eligendi exercitationem nisi mollitia qui voluptatem repudiandae corrupti. Numquam, cum tempora!
+                    </p>
+                </div>
+
+            </div>
+        </footer>
      );
 }
 
--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function OpenAccount() {
     return ( 
-        <h1> OpenAccount </h1>
+        <div className='container p-5 mb-5'>
+            <div class="row text-center">
+                <h1 className='mt-5'> Open a Stock broker account </h1>
+                <p> Modern platform and apps, ₹0 investments, and flat ₹20 intraday and F&O trades</p>
+
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width: '20%', margin: "0 auto"}}> 
+                    Sign up now 
+                </button>
+
+            </div>
+        </div>
      );
 }
 
